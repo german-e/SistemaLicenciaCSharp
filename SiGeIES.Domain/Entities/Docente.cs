1c4cf23a0341a27bd9d7d0b99046a54138f7963f
german-e <german.espindola@gmail.com>
2024-03-06 08:56:12 -0300

    Primer Commit. Estructura del Systema

---
+++ SiGeIES.Domain/Entities/Docente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGeIES.Domain.Entities
+{
+    public class Docente
+    {
+
+
+
+
+        public void InformarInasistencia()
+        {
+
+        }
+
+    }
+
+
+    
+}
---
+++ SiGeIES.Domain/Entities/NomencladorDeLicencia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiGeIES.Domain.Entities
+{
+    public class NomencladorDeLicencia
+    {
+        public Guid Id { get; set; }
+
+        public string Articulo { get; set; }
+        public string Descripcion { get; set; }
+        public int DiasPermitidos { get; set; }
+        public bool SeInformaEnIL01 { get; set; }
+    }
+}
